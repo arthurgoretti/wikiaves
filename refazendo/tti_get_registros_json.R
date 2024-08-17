7e65a673c76a520f9730feb5d9d46151d5fedcf9
arthurgoretti <arthurgoretti@gmail.com>
2024-08-17 18:38:38 -0300

    fixing tm argument in JSON

--- refazendo/tti_get_registros_json.R
+++ refazendo/tti_get_registros_json.R
@@ -1,7 +1,7 @@
 tti_get_registros_json <- function(
     id,
     p = 1,
-    rm = c("s", "f"),
+    tm = c("s", "f"),
     t = "s") {
     fromJSON_safe <- purrr::possibly(
       jsonlite::fromJSON,
@@ -15,7 +15,7 @@ tti_get_registros_json <- function(
       )
     )
 
-    glue::glue("https://www.wikiaves.com.br/getRegistrosJSON.php?tm={tm[1]}&t={t}&s={id}&o=mp&p={p}") |>
+    glue::glue("https://www.wikiaves.com.br/getRegistrosJSON.php?tm={tm}&t={t}&s={id}&o=mp&p={p}") |>
       fromJSON_safe() |>
       purrr::pluck("registros") |>
       purrr::map_if(is.null, ~"") |>
